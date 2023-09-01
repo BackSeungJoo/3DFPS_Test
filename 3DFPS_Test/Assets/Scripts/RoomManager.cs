c34898d4b97df0ead7459a91c134073c24a3b9a3
BaekSeungJoo <lueun59@gmail.com>
2023-09-01 09:14:56 +0900

    alex's 3DFPS_Prototype

---
+++ 3DFPS_Test/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager instance;
+
+    public GameObject player;
+
+    [Space]
+    public Transform spawnPoint;
+
+    [Space]
+    public GameObject roomCam;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to Server");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        Debug.Log("We're in the lobby");
+
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        Debug.Log("We,re connected and in a room!");
+
+        roomCam.SetActive(false);
+
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        _player.GetComponent<PlayerSetUp>().IsLocalPlayer();
+        _player.GetComponent<Health>().isLocalPlayer = true;
+    }
+}
